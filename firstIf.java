63a08d066d37aea5cb9061984f41e87e2ccead9d
simonedimeglio <78272736+simonedimeglio@users.noreply.github.com>
2022-11-11 14:50:48 +0100

    Primo esercizio If (11/11/22 - Simone Di Meglio)
    Primo esercizio in Java sugli IF

---
+++ firstIf.java
@@ -0,0 +1,20 @@
+class firstIf { // Dichiaro la classe
+  public static void main(String[] args) { // dichiaro il tipo
+
+    // ESERCIZIO 
+    // Crea un if che stampi
+
+    // Dichiarazione e valorizzazioni variabili
+    int x = 1;
+
+    // // IF 
+    if (x == 1) {
+      // Stampo in consolle
+      System.out.println("Hello World!");
+    } else {
+      System.out.println("Not Hello World!");
+    }
+
+
+  } // end main
+}   
