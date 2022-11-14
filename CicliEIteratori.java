7471832a2a294051fff00e3356c3ebfcc37a5cf6
simonedimeglio <78272736+simonedimeglio@users.noreply.github.com>
2022-11-14 14:59:32 +0100

    CicliEIteratori (14/11/22 SIMONE DI MEGLIO)
    Aggiunto primo esercizio volutamente in chiave semplicistica per permettere di avere un file di riferimento per la teoria riguardate cicli WHILE e DO WHILE in Java.

---
+++ CicliEIteratori.java
@@ -0,0 +1,21 @@
+class CicliEIteratori {
+  public static void main(String[] args){
+      // dichiaro variabile
+      int contatore = 1;
+
+      // while
+      while (contatore < 10) {
+          System.out.println("contatore: " + contatore);
+          contatore++;
+      } // end while
+
+
+      int contatoreDoWhile = 1;
+        do {
+            System.out.println("contatoreDoWhile: " + contatoreDoWhile);
+            contatoreDoWhile++;
+        } while (contatoreDoWhile < 5);
+
+
+  }
+}
